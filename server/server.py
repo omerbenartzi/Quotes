7625efeac4c71c1e74b98c8df8f2f954e1b885e2
Omer Ben Artzi <omerb12@gmail.com>
2020-03-24 15:41:31 +0200

    fixed py server

--- server/server.py
+++ server/server.py
@@ -1,18 +1,35 @@
-from flask import Flask, json
+from flask import Flask, jsonify, json
+from flask_cors import CORS, cross_origin
+import random
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/api/qoute')
-def get_qoute():
-    qoute = {
+PATH = 'data.json'
+
+def read_db(path):
+    try:
+        with open(PATH) as f:
+            return json.load(f)
+    except IOError:
+        return [{
         'id': 1,
         'anime': 'Test',
         'character': 'character',
         'content': 'blabasddsakfjsdaklfjsdaklfjsadlkfjsklfjklsfsa'
 
-    }
-    return json.dumps(qoute)
+    }]
 
+def get_single_qoute():
+    qoutes = read_db(PATH)
+
+    return qoutes[random.randrange(0, len(qoutes))]
+
+@app.route('/api/quote')
+@cross_origin()
+def get_quote():
+    return jsonify(get_single_qoute())
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0', 5000)
